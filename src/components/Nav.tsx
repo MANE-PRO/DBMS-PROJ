e6b9376041ace0f7d87627011031448d55149886
Himanshu Chauhan <hchauhan@aistrike.com>
2024-04-14 10:27:28 +0530

    page:search-response page added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 type P = {}
 
 const Nav: FC<P> = () => {
-  return <div className='flex items-center justify-between p-10'>
+  return <div className='flex items-center justify-between p-4'>
       <div className='flex items-center gap-3'>
         <Image src={'/nav/logo.svg'} height={60} width={60} alt='logo' />
         <p className='text-3xl font-bold text-white '>Oncoplantia</p>
